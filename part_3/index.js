7b2718ca733e5b8a1a341d230981cb23e65d6faf
Yeva Galstyan <eva.gal0908@gmail.com>
2026-01-25 17:00:51 +0100

    Exercises: 3.1-3.6: Phonebook backend

--- part_3/index.js
+++ part_3/index.js
@@ -1,8 +1,17 @@
 
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 
 app.use(express.json())
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+)
+
+morgan.token('body', (req) => {
+  return JSON.stringify(req.body)
+})
 
 let persons = [
     {

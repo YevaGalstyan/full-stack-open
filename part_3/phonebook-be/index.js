5245b732d5569ad94efe29b11080ef8ec64dffc2
Yeva Galstyan <eva.gal0908@gmail.com>
2026-02-03 16:56:27 +0100

    port added

--- part_3/phonebook-be/index.js
+++ part_3/phonebook-be/index.js
@@ -5,12 +5,12 @@ const cors = require('cors')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
 )
-app.use(cors())
 
 morgan.token('body', (req) => {
   return JSON.stringify(req.body)
@@ -99,7 +99,7 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
